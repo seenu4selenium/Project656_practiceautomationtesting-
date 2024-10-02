4e8e3a87cb57eb5cfcb5337d9566d7e4e877a285
arunamadhulika <arunamadhulika@localhost>
2024-10-02 18:28:35 -0400

    TC4GUESTUSER AUTOMATION BY Aruna

--- src/test/java/com/objectrepository/Locators.java
+++ src/test/java/com/objectrepository/Locators.java
@@ -25,10 +25,26 @@ public class Locators {
 	public final By automationpage_Homepage = By.xpath("//a[text()='Shop']");
 	public final By automationpage_products = By.xpath("//*[text()='Product Categories']");
 	public final By automationpage_SelectProduct = By.xpath("//a[text()='Android']");
-	public final By automationpage_dropdown = By.xpath("//g[@class='down']");
-	public final By filterLocator = By.xpath("//filter[@id='dropShadowTop']");
-
-	public final By automationpage_Addtocart = By.xpath("//a[text()='Add to basket']");
 	
 
+	public final By automationpage_Addtocart = By.xpath("//*[text()='Add to basket']");
+	public final By automationpage_Cartitems = By.xpath("//*[text()='1 item']");
+	public final By automationpage_Checkout = By.xpath("//a[contains(text(), 'Proceed to Checkout')]");
+	public final By automationpage_Firstname = By.id("billing_first_name");
+	public final By automationpage_Lastname = By.id("billing_last_name");
+	public final By automationpage_EmailId = By.id("billing_email");
+	public final By automationpage_Phone = By.id("billing_phone_field");
+	public final By automationpage_Billingaddr = By.id("billing_address_1");
+	public final By automationpage_Billingcity = By.id("billing_city");
+	public final By automationpage_Postcode = By.id("billing_postcode");
+	public final By automationpage_Ordersubmission = By.id("place_order");
+	public final By automationpage_Coupon = By.xpath("//a[@class='showcoupon']");
+	public final By automationpage_Apply = By.name("apply_coupon");
+	
+	
+	
+	
+	
+	
+	
 	}
--- src/test/java/com/testscenarios/TS_04_GuestUser.java
+++ src/test/java/com/testscenarios/TS_04_GuestUser.java
@@ -1,6 +1,5 @@
 package com.testscenarios;
 
-
 import org.testng.annotations.Test;
 
 import com.utilities.CommonFunctions;
@@ -10,23 +9,54 @@ public class TS_04_GuestUser extends CommonFunctions {
 	public void TS04_1() throws Exception {
 		chromeBrowserLaunch();
 		getURL("URL");
-		driver.get(p.getProperty("URL"));
-		Thread.sleep(1000);
+		clickByAnyLocator(loc.automationpage_Homepage);
 
-		driver.findElement(loc.automationpage_Homepage).click();
+		// driver.findElement(loc.automationpage_Homepage).click();
 		clickByAnyLocator(loc.automationpage_products);
 
 	}
 
 	@Test
 	public void TS04_2() throws Exception {
-		Thread.sleep(1000);
-		driver.findElement(loc.automationpage_SelectProduct).click();
-		
-		Thread.sleep(3000);
-		driver.findElement(loc.filterLocator).click();
-		
-		driver.findElement(loc.automationpage_Addtocart).click();
+
+		clickByAnyLocator(loc.automationpage_SelectProduct);
+		// scrollIntoView(loc.automationpage_Addtocart);
+		// Clickusingjavascript method is advanced when normal click does not work.
+		// clickUsingJavaScript makes element visible and click on locator
+		clickUsingJavaScript(loc.automationpage_Addtocart);
 
 	}
+
+	@Test
+	public void TS04_3() throws Exception {
+
+		clickByAnyLocator(loc.automationpage_Cartitems);
+		clickUsingJavaScript(loc.automationpage_Checkout);
+
+	}
+	
+	@Test
+	public void TS04_4() throws Exception {
+		
+		sendKeysByAnyLocator(loc.automationpage_Firstname, "automationpage_FN");
+		sendKeysByAnyLocator(loc.automationpage_Lastname, "automationpage_LN");
+		clickUsingJavaScript(loc.automationpage_EmailId);
+		sendKeysByAnyLocator(loc.automationpage_EmailId, "automationpage_EM");
+	sendKeysByAnyLocator(loc.automationpage_Phone, "automationpage_PH");
+	//	Thread.sleep(2000);
+	//	clickByAnyLocator(loc.automationpage_Phone);
+	//driver.findElement(loc.automationpage_Phone).sendKeys(p.getProperty("automationpage_PH"));
+		sendKeysByAnyLocator(loc.automationpage_Billingaddr, "automationpage_ADDR");
+		sendKeysByAnyLocator(loc.automationpage_Billingcity, "Automationpage_CITY");
+		sendKeysByAnyLocator(loc.automationpage_Postcode, "Automationpage_PostCod");
+		clickByAnyLocator(loc.automationpage_Ordersubmission);
+	}
+	
+	@Test
+	public void TS04_5() throws Exception {
+		//clickByAnyLocator(loc.automationpage_Coupon);
+		sendKeysByAnyLocator(loc.automationpage_Coupon, "automationpage_Coupon");
+		clickByAnyLocator(loc.automationpage_Apply);
+	}
+	
 }
