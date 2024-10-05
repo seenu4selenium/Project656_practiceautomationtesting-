7c75455c6c1d16b2eba7bfbae96859facdc095b3
abhinavpadigela <abhinavpadigela@abhinavs-mbp.attlocal.net>
2024-10-05 08:11:59 -0500

    sravanthi code updated

--- src/test/java/com/objectrepository/Locators.java
+++ src/test/java/com/objectrepository/Locators.java
@@ -49,8 +49,40 @@ public class Locators {
 	public final By automationpage_Coupon_editbox = By.name("coupon_code");
 	public final By automationpage_Apply = By.name("apply_coupon");
 	
+	public final By Pat_Addresses = By.xpath("//*[text()='Addresses']");
 	
+	public final By BillingAddressEdit = By.xpath("//*[@id=\"page-36\"]/div/div[1]/div/div/div[1]/header/a");
+	public final By Pat_BillingAddress_FirstName1 = By.name("billing_first_name");
+	public final By Pat_BillingAddress_LastName = By.name("billing_last_name");
+	public final By Pat_CompanyName= By.name("billing_company");
+	public final By PhoneNumber = By.name("billing_phone");
+	public final By Address = By.name("billing_address_1");
+	public final By CityName = By.name("billing_city");
+	public final By PostalCode = By.name("billing_postcode");
 	
+	public final By Billing_SaveAddressButton = By.name("save_address");
+	
+	public final By AddShippingAddress = By.xpath("(//a[text()='Edit'])[2]");
+	public final By ShippingAddress_FirstN = By.name("shipping_first_name");
+	public final By ShippingAddress_LastN = By.name("shipping_last_name");
+	public final By Shipping_Company = By.name("shipping_company");
+	public final By ShippingAddress = By.name("shipping_address_1");
+	public final By ShippingCity = By.name("shipping_city");
+	public final By ShippingPostal = By.name("shipping_postcode");
+	
+
+	public final By Shipping_SaveAddressButton = By.name("save_address");
+	
+	public final By ShippingAddressEdit = By.xpath("//*[@id=\"page-36\"]/div/div[1]/div/div/div[2]/header/a");
+
+	
+	public final By Pat_LogoutLinkText = By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[6]/a");
+	
+	public final By Pat_LoginUsername = By.name("username");
+	public final By Pat_LoginPassword = By.name("password");
+	public final By LoginButton = By.name("login");
+	
+	public final By Manage_ShipBill_AddressLink = By.linkText("shipping and billing addresses");
 	
 	
 	
